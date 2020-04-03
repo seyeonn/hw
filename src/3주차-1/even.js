cf5297f277e09c5dd83cca3822c98eb31e3153a0
seyeonn <ansse17@gmail.com>
2020-04-03 18:08:03 +0900

    ;(세미콜론) 수정

--- src/3주차-1/even.js
+++ src/3주차-1/even.js
@@ -5,7 +5,7 @@
 
 let a = [];
 for(let i=0; i<100; i++) {
-    a[i] = Math.floor(Math.random()*100 + 1)
+    a[i] = Math.floor(Math.random()*100 + 1);
 }
 for(let i=99; i>=0; i--) {
     if(a[i] % 2 == 0) {
--- src/3주차-1/sort.js
+++ src/3주차-1/sort.js
@@ -5,7 +5,7 @@
 
 let a = [];
 for(let i=0; i<100; i++) {
-    a[i] = Math.floor(Math.random()*100 + 1)
+    a[i] = Math.floor(Math.random()*100 + 1);
 }
 a.sort(compareNumber);
 console.log(a);
